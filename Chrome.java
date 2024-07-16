41fd4f79eab538722f3f183fd7c883f067ffbbf1
santoshbijjala <96513802+santoshbijjala@users.noreply.github.com>
2024-07-16 22:07:21 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,19 @@
+package Week1Day2;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta=false;
+		int releaseYear=2008;
+		char shortcutKey = 'C';
+		System.out.println("Chrome Version "+ version);
+		System.out.println("Developed by "+ developer);
+		System.out.println("Is it Beta Version "+ isBeta);
+		System.out.println("Year of release "+releaseYear);
+		System.out.println("Short Cut key "+ shortcutKey);
+	}
+
+}
